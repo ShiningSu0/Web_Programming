af3ec9f3740fe5aaec425916a72e1cdaedbcb60e
Suyeong Kim 김수영 <44190559+ShiningSu0@users.noreply.github.com>
2021-02-19 15:57:54 +0900

    CREATE LifeCycleServlet.java

---
+++ firstweb/src/examples/LifeCycleServlet.java
@@ -0,0 +1,51 @@
+package examples;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LifeCycleServlet
+ */
+@WebServlet("/LifeCycleServlet")
+public class LifeCycleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LifeCycleServlet() {
+    	System.out.println("LifeCycleServlet 생성");
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+    	System.out.println("init 생성");
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+    	System.out.println("Destroy 생성");
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    	System.out.println("service 생성");
+		// TODO Auto-generated method stub
+	}
+
+}
